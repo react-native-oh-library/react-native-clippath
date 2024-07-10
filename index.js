4f14b3b7d076811c76816c96e619172537275c75
tyBrave <tianyong21@h-partners.com>
2024-07-10 11:47:46 +0800

    chore:更新react-native-clippath 的capi版本的构建版本
    Signed-off-by: tyBrave <tianyong21@h-partners.com>

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
-// main index.js
-// import ClipPath from 'react-native-clippathview/src/ClipPath'
-// import ClipPathH from 'react-native-clippathview/src/ClipPathH'
-// export const ClipPathViewH = ClipPathH
-// export const ClipPathView = ClipPath
-
-import React from 'react';
-import ClipPathNativeComponent from './src/ClipPathNativeComponent';
-
-const ClipPath = React.forwardRef((props, ref) => {
-    const { style, ...otherProps } = props;
-    const { backgroundColor, ...otherStyle } = style;
-    let newProps = { ...otherProps, style: otherStyle };
-    if (backgroundColor) {
-        newProps.fill = backgroundColor;
-    }
-    return (
-        <ClipPathNativeComponent ref={ref} {...newProps}></ClipPathNativeComponent>
-    )
-})
-export {
-    ClipPath as ClipPathView
-}
-export default ClipPath
+import {Platfotm} from 'react-native'
+import ClipPathViewNoneAndroid from './src/ClipPathH.android.js'
+import ClipPathViewAndroid from './src/ClipPath.android.js'
+export const ClipPathView = ClipPathViewAndroid;
+export const ClipPathViewH = ClipPathViewNoneAndroid;
